c9b2c11de04d04c66a020903710efe9206601e6a
martynasvaicekauskas <martynas.vaic@gmail.com>
2017-04-12 20:58:11 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
+
+public class Controller {
+    @FXML
+    private Label skaicius;
+    @FXML
+    private TextField spejimas;
+
+    public void speti (ActionEvent e) {
+        Random random = new Random();
+        int x = random.nextInt(10)+1;
+        int y = Integer.parseInt(spejimas.getText()); // Kad Stringa pakeistu i Inta.
+        if(x==y) {
+            skaicius.setText("Saunuolis tu laimejai");
+
+        } else {
+            skaicius.setText("Neatspejai, skaicius buvo " + x);
+        }
+
+
+
+
+
+
+
+    }
+    public void baigti (ActionEvent b) {
+        System.exit(0);
+
+    }
+
+
+}
