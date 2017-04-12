679ad069858114e184c65299968114e9a22aead6
martynasvaicekauskas <martynas.vaic@gmail.com>
2017-04-12 21:19:02 +0300

    Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -18,10 +19,16 @@ public class Controller {
         int x = random.nextInt(10)+1;
         int y = Integer.parseInt(spejimas.getText()); // Kad Stringa pakeistu i Inta.
         if(x==y) {
-            skaicius.setText("Saunuolis tu laimejai");
+            // skaicius.setText("Saunuolis tu laimejai, skaicius " + x);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Saunuolis");
+            alert.show();
 
         } else {
-            skaicius.setText("Neatspejai, skaicius buvo " + x);
+           // skaicius.setText("Neatspejai, skaicius buvo " + x);
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setContentText("Ne, skaicius buvo " + x);
+            alert1.show();
         }
 
 
